490fc2e89ab19aa0a59f4242f7c37ad381b50a3e
ismaellund <40840656+ismaellund@users.noreply.github.com>
2018-11-13 10:47:18 -0200

    2.00.01 - Corrigido a capacidade de objetos em um roteiro.

--- DarkIntegrator/src/model/Roteiro.java
+++ DarkIntegrator/src/model/Roteiro.java
@@ -4,105 +4,107 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import model.Objeto.Status;
 import model.dao.DaoFactory;
 import model.dao.ObjetoDao;
 
 public class Roteiro {
 
-	private String id;
+    private String id;
 
-	private Veiculo veiculo;
+    private Veiculo veiculo;
 
-	private Date data;
+    private Date data;
 
-	private List<Objeto> objetosRoteiro = new ArrayList();
+    private List<Objeto> objetosRoteiro;
 
-	int capacidade;
+    int capacidade;
 
-	int carga = 0;
+    int carga = 0;
 
-	public Roteiro(Date data, Veiculo veiculo) {
-		this.data = data;
-		this.veiculo = veiculo;
-		this.capacidade = veiculo.getModelo().getCapacidade();
-		this.gerarRoteiro();
+    public Roteiro(Date data, Veiculo veiculo) {
+	this.data = data;
+	this.veiculo = veiculo;
+	this.capacidade = veiculo.getModelo().getCapacidade();
+	this.objetosRoteiro = new ArrayList(this.capacidade);
+	this.gerarRoteiro();
+    }
+
+    private void gerarRoteiro() {
+	try {
+	    this.adicionaPendente();
+	    this.adicionaPostado();
+	} catch (ParseException e) {
+	    e.printStackTrace();
 	}
 
-	private void gerarRoteiro() {
-		try {
-			adicionaPendente();
-			adicionaPostado();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
 
-	}
+    }
 
-	public void adicionaPendente() throws ParseException {
-		ObjetoDao objetoDao = DaoFactory.getDaoFactory().getObjetoDao();
-		for (int i = 0; capacidade >= carga && i < objetoDao.getObjetoList().size(); i++) {
-			Objeto objetoPendente = objetoDao.getObjetoList().get(i);
-			
-			if(objetoPendente.getStatus().equals(Status.PENDENTE)) {
-				objetoPendente.setStatus(Status.EM_ENTREGA);
-				objetoDao.setStatus(objetoPendente, Status.EM_ENTREGA);
-				objetosRoteiro.add(objetoPendente);
-				this.carga++;
-				
-			}
-		}
-	}
+    public void adicionaPendente() throws ParseException {
+	ObjetoDao objetoDao = DaoFactory.getDaoFactory().getObjetoDao();
+	for (int i = 0; this.capacidade > this.carga && i < objetoDao.getObjetoList().size(); i++) {
+	    Objeto objetoPendente = objetoDao.getObjetoList().get(i);
 
-	public void adicionaPostado() throws ParseException {
-		ObjetoDao objetoDao = DaoFactory.getDaoFactory().getObjetoDao();
-		for (int i = 0; capacidade >= carga && i < objetoDao.getObjetoList().size(); i++) {
-			Objeto objetoPostado = objetoDao.getObjetoList().get(i);
-			
-			if(objetoPostado.getStatus().equals(Status.POSTADO)) {
-				objetoPostado.setStatus(Status.EM_ENTREGA);
-				objetoDao.setStatus(objetoPostado, Status.EM_ENTREGA);
-				objetosRoteiro.add(objetoPostado);
-				this.carga++;
-			}
-		}
-	}
+	    if(objetoPendente.getStatus().equals(Status.PENDENTE)) {
+		objetoPendente.setStatus(Status.EM_ENTREGA);
+		objetoDao.setStatus(objetoPendente, Status.EM_ENTREGA);
+		this.objetosRoteiro.add(objetoPendente);
+		this.carga++;
 
-	public String getId() {
-		return this.id;
+	    }
 	}
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void adicionaPostado() throws ParseException {
+	ObjetoDao objetoDao = DaoFactory.getDaoFactory().getObjetoDao();
+	for (int i = 0; this.capacidade > this.carga && i < objetoDao.getObjetoList().size(); i++) {
+	    Objeto objetoPostado = objetoDao.getObjetoList().get(i);
 
-	public Veiculo getVeiculo() {
-		return this.veiculo;
+	    if(objetoPostado.getStatus().equals(Status.POSTADO)) {
+		objetoPostado.setStatus(Status.EM_ENTREGA);
+		objetoDao.setStatus(objetoPostado, Status.EM_ENTREGA);
+		this.objetosRoteiro.add(objetoPostado);
+		this.carga++;
+	    }
 	}
+    }
 
-	public void setVeiculo(Veiculo veiculo) {
-		this.veiculo = veiculo;
-	}
+    public String getId() {
+	return this.id;
+    }
 
-	public Date getData() {
-		return this.data;
-	}
+    public void setId(String id) {
+	this.id = id;
+    }
 
-	public void setData(Date data) {
-		this.data = data;
-	}
+    public Veiculo getVeiculo() {
+	return this.veiculo;
+    }
 
-	public List<Objeto> getObjetosRoteiro() {
-		return this.objetosRoteiro;
-	}
+    public void setVeiculo(Veiculo veiculo) {
+	this.veiculo = veiculo;
+    }
 
-	public void setObjetosRoteiro(List<Objeto> objetosRoteiro) {
-		this.objetosRoteiro = objetosRoteiro;
-	}
+    public Date getData() {
+	return this.data;
+    }
 
-	@Override
-	public String toString() {
-		return "Veículo: " + this.veiculo.toString();
-	}
+    public void setData(Date data) {
+	this.data = data;
+    }
+
+    public List<Objeto> getObjetosRoteiro() {
+	return this.objetosRoteiro;
+    }
+
+    public void setObjetosRoteiro(List<Objeto> objetosRoteiro) {
+	this.objetosRoteiro = objetosRoteiro;
+    }
+
+    @Override
+    public String toString() {
+	return "Veículo: " + this.veiculo.toString();
+    }
 }
--- DarkIntegrator/src/resources/AppConsts.java
+++ DarkIntegrator/src/resources/AppConsts.java
@@ -2,39 +2,33 @@ package resources;
 
 public class AppConsts {
 
-	public static final String CAMINHO = "http://localhost:8080/integrador";
+    public static final String CAMINHO = "http://localhost:8080/integrador";
 
-	public static boolean BANCO;
+    public static boolean BANCO;
 
-	// *****************************************************
-	// ATALHO PARA COMENTAR VARIAS LINHAS: CTRL + SHIFT + C
-	// *****************************************************
+    // *****************************************************
+    // ATALHO PARA COMENTAR VARIAS LINHAS: CTRL + SHIFT + C
+    // *****************************************************
 
-	// WINDOWS
+    // WINDOWS
 
-	public static final String CAMINHO_RECIBO = "C:/TAP/recibo.txt";
+    public static final String CAMINHO_RECIBO = "C:/TAP/recibo.txt";
+    public static final String CAMINHO_OBJETO = "C:/TAP/lstObjeto.json";
+    public static final String CAMINHO_MOTORISTA = "C:/TAP/lstMotorista.json";
+    public static final String CAMINHO_VEICULO = "C:/TAP/lstVeiculo.json";
+    public static final String CAMINHO_USUARIO = "C:/TAP/lstUsuario.json";
+    public static final String CAMINHO_MODELO = "C:/TAP/lstModelo.json";
+    public static final String CAMINHO_ROTEIRO = "C:/TAP/lstRoteiro.json";
 
-	public static final String CAMINHO_OBJETO = "C:/TAP/lstObjeto.json";
+    // LINUX
 
-	public static final String CAMINHO_MOTORISTA = "C:/TAP/lstMotorista.json";
-
-	public static final String CAMINHO_VEICULO = "C:/TAP/lstVeiculo.json";
-
-	public static final String CAMINHO_USUARIO = "C:/TAP/lstUsuario.json";
-
-	public static final String CAMINHO_MODELO = "C:/TAP/lstModelo.json";
-
-	public static final String CAMINHO_ROTEIRO = "C:/TAP/lstRoteiro.json";
-
-	// LINUX
-
-	//	public static final String CAMINHO_RECIBO = "/home/USUARIO/TAP/recibo.txt";
-	//	public static final String CAMINHO_OBJETO = "/home/USUARIO/TAP/lstObjeto.json";
-	//	public static final String CAMINHO_MOTORISTA = "/home/USUARIO/TAP/lstMotorista.json";
-	//	public static final String CAMINHO_VEICULO = "/home/USUARIO/TAP/lstVeiculo.json";
-	//	public static final String CAMINHO_LOGIN = "/home/USUARIO/TAP/lstLogin.json";
-	//	public static final String CAMINHO_MODELO = "/home/USUARIO/lstModelo.json";
-	//	public static final String CAMINHO_DEPOSITO_PENDENTE = "/home/USUARIO/lstDepositoPendente.json";
-	//	public static final String CAMINHO_DEPOSITO = "/home/USUARIO/lstDeposito.json";
-	//	public static final String CAMINHO_ROTEIRO = "/home/USUARIO/lstRoteiro.json;
+    //public static final String CAMINHO_RECIBO = "/home/USUARIO/TAP/recibo.txt";
+    //public static final String CAMINHO_OBJETO = "/home/USUARIO/TAP/lstObjeto.json";
+    //public static final String CAMINHO_MOTORISTA = "/home/USUARIO/TAP/lstMotorista.json";
+    //public static final String CAMINHO_VEICULO = "/home/USUARIO/TAP/lstVeiculo.json";
+    //public static final String CAMINHO_LOGIN = "/home/USUARIO/TAP/lstLogin.json";
+    //public static final String CAMINHO_MODELO = "/home/USUARIO/lstModelo.json";
+    //public static final String CAMINHO_DEPOSITO_PENDENTE = "/home/USUARIO/lstDepositoPendente.json";
+    //public static final String CAMINHO_DEPOSITO = "/home/USUARIO/lstDeposito.json";
+    //public static final String CAMINHO_ROTEIRO = "/home/USUARIO/lstRoteiro.json";
 }
